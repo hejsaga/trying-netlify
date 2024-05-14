4e0310ff5a2384c7378f399309e4128789d9bcbe
Saga Swahn <sagaswahn@hotmail.com>
2024-05-14 13:01:01 +0200

    Removed secrets

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -4,12 +4,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD6WioiELMf5Cmr0syLK7z7c-Uak8fVZhA",
-  authDomain: "authentication-9c05e.firebaseapp.com",
-  projectId: "authentication-9c05e",
-  storageBucket: "authentication-9c05e.appspot.com",
-  messagingSenderId: "853654734780",
-  appId: "1:853654734780:web:6f93af0fdc37d68c804db9",
+  // your config goes here
 };
 
 // Initialize Firebase
